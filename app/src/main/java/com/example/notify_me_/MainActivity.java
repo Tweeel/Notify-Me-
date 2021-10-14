3026024d1d8518f55e7489340a6d3c1659341bfd
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-10-14 10:58:11 +0100

    Create a notification channel

--- app/src/main/java/com/example/notify_me_/MainActivity.java
+++ app/src/main/java/com/example/notify_me_/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.notify_me_;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +13,9 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button_notify;
+
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotifyManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +33,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendNutification(){
 
+    }
+
+    public void createNotificationChannel(){
+        mNotifyManager =(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
+
+            // Create a NotificationChannel
+            NotificationChannel notificationChannel =  new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification",NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+
+        }
     }
 }
