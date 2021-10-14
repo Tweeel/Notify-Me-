97ae59f70fe2cd759277a1db44e4c71aec7fa4b2
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-10-14 10:36:07 +0100

    app creation

---
+++ app/src/main/java/com/example/notify_me_/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.notify_me_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button_notify;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void sendNutification(){
+
+    }
+}
