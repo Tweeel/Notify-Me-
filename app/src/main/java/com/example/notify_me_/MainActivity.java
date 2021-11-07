2a634e5c4d6c22d13f26309f8d677ee9c95342f8
Baba Bendermel Houssem Eddine <75279465+Tweeel@users.noreply.github.com>
2021-11-07 22:10:17 +0100

    add the notification and add the intent to the notification

--- app/src/main/java/com/example/notify_me_/MainActivity.java
+++ app/src/main/java/com/example/notify_me_/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.notify_me_;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -14,8 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     private Button button_notify;
 
+
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private NotificationManager mNotifyManager;
+    private static final int NOTIFICATION_ID = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +29,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button_notify = findViewById(R.id.notify);
-        button_notify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        createNotificationChannel();
     }
 
-    public void sendNutification(){
-
+    public void sendNotification(View view) {
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
     }
 
     public void createNotificationChannel(){
         mNotifyManager =(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
-
             // Create a NotificationChannel
             NotificationChannel notificationChannel =  new NotificationChannel(PRIMARY_CHANNEL_ID,
                     "Mascot Notification",NotificationManager.IMPORTANCE_HIGH);
@@ -48,7 +49,21 @@ public class MainActivity extends AppCompatActivity {
             notificationChannel.enableVibration(true);
             notificationChannel.setDescription("Notification from Mascot");
             mNotifyManager.createNotificationChannel(notificationChannel);
-
         }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Build the notification with all of the parameters.
+        return new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText(getString(R.string.notification_text))
+                .setSmallIcon(R.drawable.ic_android_foreground)
+                .setAutoCancel(true).setContentIntent(notificationPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
     }
 }
